3c0a61247f90b53f9dcacc8fc97f04aa32be7e1e
SebastianPagacz <234595@student.uek.krakow.pl>
2025-09-28 18:46:56 +0200

    Fronted pokemon images + dokcer-compose

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,5 +1,5 @@
 async function getData(){
-    const url = "https://localhost:32771/api/Pokemon";
+    const url = "https://localhost:8081/api/Pokemon";
 
     try{
         const response = await fetch(url,{
@@ -23,16 +23,27 @@ async function getData(){
 async function renderData(){
     let requestedData = await getData();
     const dataContainer = document.getElementById("container");
+    
     for (const item of requestedData){
-        let boxElem = document.createElement("span");
+        let boxElem = document.createElement("div");
+        let image = document.createElement("img");
         let idElem = document.createElement("p");
         let nameElem = document.createElement("p");
         
+        const pokeImg = await fetch(`https://pokeapi.co/api/v2/pokemon/${item.id}/`,{
+            method: "GET"
+        });
+        const imgData = await pokeImg.json();
+        image.src = imgData.sprites.front_default;
+        image.alt = item.name;
+
+        image.className = "poke-image";
 
         idElem.innerText = item.id;
         nameElem.innerText = item.name;
 
         dataContainer.appendChild(boxElem);
+        boxElem.appendChild(image);
         boxElem.appendChild(idElem);
         boxElem.appendChild(nameElem);
     }
